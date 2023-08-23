531d60fecb8ce8f96f9c7f8fabae511e885c2a60
_Mycola_ <litvinenkomycola263@ukr.net>
2023-08-23 15:30:18 +0300

    header

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 "use strict";
+const searchInput = document.querySelector('.search__input');
+const burgerMenu = document.querySelector('.header__burger');
+const menu = document.querySelector('.nav-header-right');
+document.addEventListener('click', (e)=>{
+    if(e.target.closest('.search__input')){
+        searchInput.parentElement.classList.add('__active')
+    }else{
+        searchInput.parentElement.classList.remove('__active')
+    }
+})
+burgerMenu.addEventListener('click', ()=>{
+    burgerMenu.classList.toggle('__active');
+    menu.classList.toggle('__active');
+
+})
