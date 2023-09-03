fa40b782ad69c806de51f8a051dfda20cdc8c2df
_Mycola_ <litvinenkomycola263@ukr.net>
2023-09-03 15:34:50 +0300

    add to cart by button interactive

--- data/data.js
+++ data/data.js
@@ -1,6 +1,7 @@
 const products = [
     {
-    img: "https://images.unsplash.com/photo-1595861021888-e8192a7f774e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80",
+      id: "e47638ce-6aa0-4b85-b27f-e1d07eb671d2",
+      img: "https://images.unsplash.com/photo-1595861021888-e8192a7f774e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80",
       name: 'Basketball ball',
       rating : {
           stars: 4.5,
@@ -9,6 +10,7 @@ const products = [
       priceCents: 5035,
     },
     {
+      id: "e47638ce-6aa0-4b85-b27f-e1d07eb671d1",
       img: "https://images.unsplash.com/photo-1485965120184-e220f721d03e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
         name: 'Gravel',
         rating : {
@@ -18,6 +20,7 @@ const products = [
         priceCents: 67790,
     },
     {
+      id: "e47638ce-6aa0-4b85-b27f-e1d07eb678c9",
       img: "https://plus.unsplash.com/premium_photo-1684952850099-396e7183bc6c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1931&q=80",
         name: 'White socks with rainbow lines',
         rating : {
@@ -27,6 +30,7 @@ const products = [
         priceCents: 985,
     },
     {
+      id: "e47638ce-6aa0-4b85-b27f-e1d07eb678c8",
       img: "https://images.unsplash.com/photo-1539870113056-c6d7439045ce?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2132&q=80",
         name: 'Rainbow paper clips',
         rating : {
@@ -36,6 +40,7 @@ const products = [
         priceCents: 20,
     },
     {
+      id: "e47638ce-6aa0-4b85-b27f-e1d07eb678c7",
       img: "https://plus.unsplash.com/premium_photo-1663036970563-99624abc950e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
         name: 'Washing machine',
         rating : {
@@ -45,6 +50,7 @@ const products = [
         priceCents: 31045,
     },
     {
+      id: "e47638ce-6aa0-4b85-b27f-e1d07eb678c6",
       img: "https://images.unsplash.com/photo-1514651029128-173d2e6ea851?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1970&q=80",
         name: 'Glass for luxury drink',
         rating : {
