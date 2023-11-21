2931ee49196321fc86d47a5e7ec17a451923db2b
_Mycola_ <litvinenkomycola263@ukr.net>
2023-11-21 23:45:46 +0200

    add array delivery options fixed bug with space in cart__quantity  first functional of delivery options  uses library dayjs()

--- data/cart.js
+++ data/cart.js
@@ -4,14 +4,20 @@ export let cart = JSON.parse(localStorage.getItem("cart")) || [
     {
         productId: "e47638ce-6aa0-4b85-b27f-e1d07eb671d1",
         quantity: 1,
+        price: 999,
+        deliveryOptionId: "1",
     },
     {
         productId: "e47638ce-6aa0-4b85-b27f-e1d07eb671d2",
         quantity: 6,
+        price: 999,
+        deliveryOptionId: "1",
     },
     {
         productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
         quantity: 3,
+        price: 999,
+        deliveryOptionId: "1",
     },
 ];
 export function addToCart(productId, productPrice) {
@@ -30,6 +36,7 @@ export function addToCart(productId, productPrice) {
             productId: productId,
             quantity: 1,
             price: productPrice,
+            deliveryOptionId: "1",
         });
     }
     saveToStorage();
