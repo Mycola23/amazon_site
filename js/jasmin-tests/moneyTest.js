d29012ed153ee199fc5097df506500fd58cf1405
_Mycola_ <litvinenkomycola263@ukr.net>
2024-02-20 22:33:36 +0200

    create tests for fun formatMoney by jasmine

--- js/jasmin-tests/moneyTest.js
+++ js/jasmin-tests/moneyTest.js
@@ -4,4 +4,15 @@ describe("test suite: formatMoney", () => {
     it("converts cents into dollars", () => {
         expect(formatMoneys(2095)).toEqual("20.95");
     });
+    it("works with 0 ", () => {
+        expect(formatMoneys(0)).toEqual("0.00");
+    });
+    describe("rounding", () => {
+        it("rounds up to the nearest cent", () => {
+            expect(formatMoneys(2000.5)).toEqual("20.01");
+        });
+        it("rounds down to the nearest cent", () => {
+            expect(formatMoneys(2000.4)).toEqual("20.00");
+        });
+    });
 });
